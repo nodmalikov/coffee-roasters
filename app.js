e73c29793e9022538a3a8035c73fbf011f77c80a
nodmalikov <abcmalikov@gmail.com>
2024-08-15 19:45:38 +0500

    Add to plan page modal

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const elsAccordionHead = document.querySelectorAll('.faq-accordion-head');
+const elJsModalPlan = document.querySelector('.js-modal-plan');
+const elJsButtonFaq = document.querySelector('.js-button-faq');
+const elJsFooterModalButton = document.querySelector('.js-modal-footer-button');
 
 if (elsAccordionHead) {
     elsAccordionHead.forEach(function (elAccordionHead) {
         elAccordionHead.addEventListener('click', function () {
-            // Barcha ochiq accordionlarni yopish
+            // BARCHA OCHIQ ACCORDIONLARNI YOPISH
             elsAccordionHead.forEach(function (head) {
                 const body = head.nextElementSibling;
                 const icon = head.querySelector('.faq-accordion-icon');
@@ -19,5 +22,17 @@ if (elsAccordionHead) {
             elAccordionBody.classList.toggle('open-accordion');
             elAccordionIcon.classList.toggle('faq-accordion-icon-rotate');
         });
+    });
+}
+
+if (elJsButtonFaq) {
+    elJsButtonFaq.addEventListener('click', function () {
+        elJsModalPlan.classList.add('open-modal');
+    });
+}
+
+if (elJsFooterModalButton) {
+    elJsFooterModalButton.addEventListener('click', function () {
+        elJsModalPlan.classList.remove('open-modal');
     });
 }
